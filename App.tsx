76fbc81ed392cc21e0957d309059f3e1e99ed194
Lev Usenko <lyusen@yahoo.com>
2022-02-21 01:04:33 +0300

    Добавлена навигация

--- App.tsx
+++ App.tsx
@@ -1,20 +1,52 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Button, StyleSheet, Text, View } from "react-native";
+
+const Stack = createNativeStackNavigator();
+
+const ProfileScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Profile Screen</Text>
+      <Button
+        title="Go to Skills"
+        onPress={() => navigation.navigate("Skills")}
+      />
+    </View>
+  );
+};
+
+const SkillsScreen = ({ navigation }: { navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Skills Screen</Text>
+      <Button
+        title="Go to Profile"
+        onPress={() => navigation.navigate("Profile")}
+      />
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Skills" component={SkillsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 32,
+    color: "#ff0000",
   },
 });
