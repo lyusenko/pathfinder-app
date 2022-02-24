f7473338f9ae4fc41761e06c4309198dfaecd434
kirbro19 <kirich19@gmail.com>
2022-02-24 20:43:38 +0300

    Исправлена ошибка сохранения полей у профиля

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -16,73 +16,73 @@ export const ProfileScreen = () => {
       <Input
         label="Имя персонажа"
         defaultValue={values.characterName}
-        onChangeText={handleEndEditing(ProfileKey.CharacterName)}
+        onEndEditing={handleEndEditing(ProfileKey.CharacterName)}
         placeholder="Имя персонажа"
       />
       <Input
         label="Уровень"
         defaultValue={values.level}
-        onChangeText={handleEndEditing(ProfileKey.Level)}
+        onEndEditing={handleEndEditing(ProfileKey.Level)}
         placeholder="Уровень"
       />
       <Input
         label="Пункты героизма"
         defaultValue={values.heroismPoints}
-        onChangeText={handleEndEditing(ProfileKey.HeroismPoints)}
+        onEndEditing={handleEndEditing(ProfileKey.HeroismPoints)}
         placeholder="Пункты героизма"
       />
       <Input
         label="Опыт"
         defaultValue={values.experience}
-        onChangeText={handleEndEditing(ProfileKey.Experience)}
+        onEndEditing={handleEndEditing(ProfileKey.Experience)}
         placeholder="Опыт"
       />
       <Input
         label="Народ и родословная"
         defaultValue={values.ancestry}
-        onChangeText={handleEndEditing(ProfileKey.Ancestry)}
+        onEndEditing={handleEndEditing(ProfileKey.Ancestry)}
         placeholder="Народ и родословная"
       />
       <Input
         label="Класс"
         defaultValue={values.class}
-        onChangeText={handleEndEditing(ProfileKey.Class)}
+        onEndEditing={handleEndEditing(ProfileKey.Class)}
         placeholder="Класс"
       />
       <Input
         label="Размер"
         defaultValue={values.size}
-        onChangeText={handleEndEditing(ProfileKey.Size)}
+        onEndEditing={handleEndEditing(ProfileKey.Size)}
         placeholder="Размер"
       />
       <Input
         label="Мировоззрение"
         defaultValue={values.ideology}
-        onChangeText={handleEndEditing(ProfileKey.Ideology)}
+        onEndEditing={handleEndEditing(ProfileKey.Ideology)}
         placeholder="Мировоззрение"
       />
       <Input
         label="Дескрипторы"
         defaultValue={values.descriptors}
-        onChangeText={handleEndEditing(ProfileKey.Descriptors)}
+        onEndEditing={handleEndEditing(ProfileKey.Descriptors)}
         placeholder="Дескрипторы"
       />
       <Input
         label="Божество"
         defaultValue={values.deity}
-        onChangeText={handleEndEditing(ProfileKey.Deity)}
+        onEndEditing={handleEndEditing(ProfileKey.Deity)}
         placeholder="Божество"
       />
       <Input
         label="Умения в оружии"
         defaultValue={values.weaponSkills}
-        onChangeText={handleEndEditing(ProfileKey.WeaponSkills)}
+        onEndEditing={handleEndEditing(ProfileKey.WeaponSkills)}
         placeholder="Умения в оружии"
       />
       <Input
         label="Языки"
         defaultValue={values.languages}
-        onChangeText={handleEndEditing(ProfileKey.Languages)}
+        onEndEditing={handleEndEditing(ProfileKey.Languages)}
         placeholder="Языки"
       />
     </ScrollView>
